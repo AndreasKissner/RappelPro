0f99bf745066055b30e542baaccdd6a48b28533c
Andreas Kissner <andikiss500@gmail.com>
2025-12-31 16:17:53 +0100

    Added EU time line with change in app.config.ts new Modules

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,15 +1,26 @@
-import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZoneChangeDetection, provideZonelessChangeDetection } from '@angular/core';
+import { 
+  ApplicationConfig, 
+  provideBrowserGlobalErrorListeners, 
+  provideZonelessChangeDetection, 
+  LOCALE_ID, 
+  importProvidersFrom // Hinzugefügt für stabilere Modul-Einbindung
+} from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
 import { getFirestore, provideFirestore } from '@angular/fire/firestore';
 
-
-
 import { provideNativeDateAdapter } from '@angular/material/core';
+// Wir nutzen das Modul direkt, falls der "provideNativeMatTimeAdapter" zickt
+import { MatTimepickerModule } from '@angular/material/timepicker'; 
 import { firebaseConfig } from './configFirebase/firebase-config';
 
+// Wichtig für europäische Formate:
+import { registerLocaleData } from '@angular/common';
+import localeDe from '@angular/common/locales/de'; 
+
+registerLocaleData(localeDe);
 
 export const appConfig: ApplicationConfig = {
   providers: [
@@ -18,7 +29,15 @@ export const appConfig: ApplicationConfig = {
     provideRouter(routes),
     provideFirebaseApp(() => initializeApp(firebaseConfig)),
     provideFirestore(() => getFirestore()),
+    
+    // Adapter für Datum
     provideNativeDateAdapter(),
     
+    // Sicherer Weg für den Timepicker in deiner Version:
+    importProvidersFrom(MatTimepickerModule), 
+
+    // Hier setzen wir die Sprache global auf Deutsch
+    // Das sorgt für 24h-Format (kein AM/PM) und dd.MM.yyyy
+    { provide: LOCALE_ID, useValue: 'de-DE' }
   ]
 };
